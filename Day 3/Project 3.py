0994794f2976994d43d9f0b58cfe1e0bc0f01760
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-12-18 21:49:22 +0000

    Modified for three switches

--- Day 3/Project 3.py
+++ Day 3/Project 3.py
@@ -1,11 +1,10 @@
-# Imports
 from machine import Pin
 import time
 
-# Project 3
-button1 = Pin(13, Pin.IN, Pin.PULL_DOWN)
-button2 = Pin(8, Pin.IN, Pin.PULL_DOWN)
-button3 = Pin(3, Pin.IN, Pin.PULL_DOWN)
+# Button names and GPIO pin numbers
+button1 = Pin(13, Pin.IN, Pin.PULL_DOWN)  # Red
+button2 = Pin(8, Pin.IN, Pin.PULL_DOWN)   # Green
+button3 = Pin(3, Pin.IN, Pin.PULL_DOWN)   # Amber
 
 # LED names and GPIO pin numbers
 red = Pin(18, Pin.OUT)
@@ -18,28 +17,43 @@ def blink(led, count=1, on_duration=0.5, off_duration=0.5):
         led.value(1)
         time.sleep(on_duration)
         led.value(0)
-        if _ < count - 1:
-            time.sleep(off_duration)
+        time.sleep(off_duration)
+
+def traffic_light_sequence():
+    """Simulate traffic light sequence."""
+    red.value(1)
+    time.sleep(1)
+    red.value(0)
+    amber.value(1)
+    time.sleep(1)
+    amber.value(0)
+    green.value(1)
+    time.sleep(1)
+    green.value(0)
 
 while True:
-    
-    time.sleep(0.2)
-        
-    if button1.value() == 1 and button2.value() == 1: # Both
-        print("Buttons 1 and 2 pressed")
-        blink(red, count=3, on_duration=0.5, off_duration=0.2)
-        green.value(1) 
-        time.sleep(1)
-        green.value(0)
+    time.sleep(0.2)  # Delay
 
-    elif button1.value() == 1: # If button 1 is pressed
-        
-        print("Button 1 pressed")
-        green.value(1) # amber LED on
-        red.value(0) # red LED off
+    # Check each button and log if pressed
+    if button1.value() == 1:
+        print("Button 1 (Red) pressed")
+        red.value(1)  # Red on
+    else:
+        red.value(0)  # Red off
 
-    else: # If no buttons are being pressed
-        
-        red.value(1) # red LED on
-        amber.value(0) # amber LED off
-        green.value(0) # green LED off
+    if button2.value() == 1:
+        print("Button 2 (Green) pressed")
+        green.value(1)  # Green on
+    else:
+        green.value(0)  # Green off
+
+    if button3.value() == 1:
+        print("Button 3 (Amber) pressed")
+        amber.value(1)  # Amber on
+    else:
+        amber.value(0)  # Amber off
+
+    # Traffic light sequence for simultaneous button press
+    if button1.value() == 1 and button2.value() == 1 and button3.value() == 1:
+        print("All three buttons pressed, initiating traffic light sequence")
+        traffic_light_sequence()
